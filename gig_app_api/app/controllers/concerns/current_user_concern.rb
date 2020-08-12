e7805dad7c6e88f11c99e0a1eb563df5dee3997a
Tyler Davis <tylerdavis@Tylers-iMac-2.local>
2020-08-12 09:54:31 -0400

    User auth complete.

--- gig_app_api/app/controllers/concerns/current_user_concern.rb
+++ gig_app_api/app/controllers/concerns/current_user_concern.rb
@@ -4,8 +4,9 @@ module CurrentUserConcern
     included do
         before_action :set_current_user
     end
+
     def set_current_user
-        if session [:user_id]
+        if session[:user_id]
             @current_user = User.find(session[:user_id])
         end
     end
--- gig_app_api/config/initializers/session_store.rb
+++ gig_app_api/config/initializers/session_store.rb
@@ -1,6 +1,5 @@
-
-if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_gig_app_api", domain: 
-else
+    if Rails.env == "production"
     Rails.application.config.session_store :cookie_store, key: "_gig_app_api"
-end
+    else
+    Rails.application.config.session_store :cookie_store, key: "_gig_app_api"
+    end
