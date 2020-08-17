8e6c5332e6df50a8e0aca86d80dafb27139f24cf
Tyler Davis <tylerdavis@Tylers-iMac-2.local>
2020-08-16 20:53:32 -0400

    Changed welcome message

--- gig_app_api/app/controllers/welcome_controller.rb
+++ gig_app_api/app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
     def index
-        render json: { status: 200, message: "Noticeboard API" }
+        render json: { status: 200, message: "Gig App Api" }
       end
 end
